04f90b1fa4f3a2e5da461dd94f0f2ab0321c7c2e
Mahima-codes11 <bajajmahima11@gmail.com>
2026-08-12 22:17:09 +0530

    Add files via upload

---
+++ day1/day1q1.c
@@ -0,0 +1,12 @@
+// WAP to  calculate sum of two nos
+
+
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter two nos");
+ scanf("%d%d",&a,&b);
+ printf("Sum is:- %d",a+b);
+ return 0;
+}
---
+++ day1/day1q2.c
@@ -0,0 +1,14 @@
+//WAP to calculate sum,product,difference and quotient of two nos:
+ 
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter two nos");
+ scanf("%d%d",&a,&b);
+ printf("Sum is:- %d \n",a+b);
+ printf("Difference is:- %d \n",a-b);
+ printf("Product is:- %d \n",a*b);
+ printf("Quotient is:- %d \n",a/b);
+ return 0;
+}
