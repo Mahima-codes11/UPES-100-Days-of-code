62916b3532e69c3e9a2b4b233faddaab78802bfc
Mahima-codes11 <bajajmahima11@gmail.com>
2026-08-15 15:12:11 +0530

    Add files via upload

---
+++ day6/day6q2.c
@@ -0,0 +1,24 @@
+//WAP to input an integer and check whether it is positive,negative or zero using nested if else
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter an integer");
+  scanf("%d",&a);
+  if(a>0)
+   {
+     if(a==0)
+      {
+        printf("%d is zero",a);
+      }
+     else
+      {
+        printf("%d is positive",a);
+      }
+    }
+  else
+    {
+      printf("%d is negative",a);
+    }
+   return 0;
+}
