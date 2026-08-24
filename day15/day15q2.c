5406273db002ac4a5979bbe732944cbd8b2b9eb0
Mahima-codes11 <bajajmahima11@gmail.com>
2026-08-24 15:32:57 +0530

    Add files via upload

---
+++ day15/day15q2.c
@@ -0,0 +1,15 @@
+ //Write a program to reverse a given number.
+#include<stdio.h>
+int main()
+{
+ int n,rev=0;
+ printf("Enter a no");
+ scanf("%d",&n);
+ while(n>0)
+ {
+  rev=rev*10+n%10;
+  n=n/10;
+ }
+ printf("Revese = %d",rev);
+ return 0;
+}
