13977558566fd17b8fe669887424324bce2c0e3d
Mahima-codes11 <bajajmahima11@gmail.com>
2026-09-01 23:17:53 +0530

    Add files via upload

---
+++ day23/day23q2.c
@@ -0,0 +1,23 @@
+/*Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+#include<stdio.h>
+int main()
+{
+ int i,j;
+ char a;
+ a='*';
+ for(i=0;i<=4;i++)
+  {
+   for(j=0;j<=4;j++)
+   {
+    printf("%c",a);
+   }
+   printf("\n");
+  }
+ return 0;
+}
