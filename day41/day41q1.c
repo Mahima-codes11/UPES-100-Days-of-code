638d64970eeb1b8a4050661603b006040a26d1f9
Mahima-codes11 <bajajmahima11@gmail.com>
2026-09-19 23:05:19 +0530

    Add files via upload

---
+++ day41/day41q1.c
@@ -0,0 +1,16 @@
+//Count characters in a string without using built-in length functions.
+#include<stdio.h>
+int main()
+{
+ char str[100];
+ int i=0,count=0;
+ printf("Enter a string");
+ scanf("%s",str);
+ while(str[i] != '\0')
+  {
+   count ++;
+   i++;
+  }
+ printf("No of characters in string %d",count);
+ return 0;
+}
---
+++ day41/day41q2.c
@@ -0,0 +1,16 @@
+//Print each character of a string on a new line.
+#include<stdio.h>
+int main()
+{
+ int i=0;
+ char str[100];
+ printf("Enter a string");
+ scanf("%s",str);
+ while(str[i]!= '0')
+ {
+   printf("%c\n",str[i]);
+   i++;
+ }
+
+ return 0;
+}
