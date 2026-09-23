8287b640bfe8e5d93ab71391df121d8831f8d0bc
Mahima-codes11 <bajajmahima11@gmail.com>
2026-09-23 20:38:54 +0530

    Add files via upload

---
+++ day45/day45q2.c
@@ -0,0 +1,22 @@
+//Toggle case of each character in a string.
+#include<stdio.h>
+int main()
+{
+ char str[100];
+ int i;
+ printf("Enter a string");
+ scanf("%s",str);
+ for(i=0;str[i]!='\0';i++)
+ {
+   if(str[i]>='a' && str[i]<='z')
+   {
+    str[i]=str[i]-32;
+   }
+  else if(str[i]>='A' && str[i]<='A')
+   {
+    str[i]=str[i]+32;
+   }
+ }
+ printf("Toggle case %s",str);
+ return 0;
+}
